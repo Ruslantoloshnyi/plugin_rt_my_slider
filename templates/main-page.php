b338e6f8cd6f4597dd2d820fdda44d52c3e65d8e
Ruslan <ru@skylinehealthgroup.com>
2023-04-24 18:12:38 +0300

    add css style to rt_my_slider_style.css

--- templates/main-page.php
+++ templates/main-page.php
@@ -1,16 +1,18 @@
 <section class="rt-slider-section">
 
-    <div class="rt_slider_container">
+    <div class="rt-slider-container">
 
         <div class="rt-slider-heading">
             <h1>RT My Slider</h1>
         </div>
 
-        <form id="upload-form" method="post" enctype="multipart/form-data">
-            <label for="file-upload">Select file to upload:</label>
-            <input type="file" id="file-upload" name="file">
-            <button type="submit" name="submit">Upload</button>
-        </form>
+        <div class="rt-slider-form">
+            <form id="upload-form" method="post" enctype="multipart/form-data">
+                <label for="file-upload">Select file to upload:</label>
+                <input type="file" id="file-upload" name="file">
+                <button type="submit" name="submit">Upload</button>
+            </form>
+        </div>
 
         <div class="rt-slider-review-image">
             <div class="your">Your images :</div>
@@ -19,7 +21,7 @@
 
         <?php require_once(RT_SLIDER__PLUGIN_DIR . 'templates/uploads.php'); ?>
 
-        <label for="slider-select">check slider</label>
+        <label class="rt-check-slider-label" for="slider-select">Check slider</label>
         <select name="slider-select" id="slider-select">
             <option value="none">none</option>
             <option value="slider-with-controls">slider-with-controls</option>
@@ -32,10 +34,11 @@
             <img id="" src="<?php echo RT_SLIDER__PLUGIN_URL . 'assets/slider-img/Slider-with-controls.jpg'; ?>" alt="">
         </div>
 
-        <div class="your">Your slider :</div>
-
-        <?php echo do_shortcode('[rt_slider_with_controls]'); ?>
-        <?php echo do_shortcode('[rt_slider_with_indicators]'); ?>
+        <div class="rt-slider-show">
+            <div class="your">Your slider :</div>
+            <?php echo do_shortcode('[rt_slider_with_controls]'); ?>
+            <?php echo do_shortcode('[rt_slider_with_indicators]'); ?>
+        </div>
 
         <div class="rt-slider-shortcode">
             <div class="your">Your shortcode :</div>
